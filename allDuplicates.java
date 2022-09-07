666e7661dd8e6b9002a3abde5d552bc3a5bbd5dc
anildev26 <anilsahudev26@gmail.com>
2022-09-07 23:56:38 +0530

    Find All Duplicates in an Array code file [updated]

--- allDuplicates.java
+++ allDuplicates.java
@@ -1,5 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class allDuplicates {
     public static void main(String[] args) {
+        int[] nums = {4,3,2,7,8,2,3,1};
+        System.out.println(findDuplicates(nums));
 
     }
+         static List<Integer> findDuplicates(int[] nums) {
+            ArrayList<Integer> res = new ArrayList<>();
+            for(int i = 0;i<nums.length; i++){
+                int assumeIndex = Math.abs(nums[i]) -1;
+                if(nums[assumeIndex] < 0){
+                    res.add(Math.abs(nums[i]));
+                }
+                nums[assumeIndex] = -nums[assumeIndex];
+            }
+            return res;
+
+        }
+
 }
